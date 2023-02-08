94b8d8cef15a5bc98af469c574e19439e1dbc114
Mayank0909 <mayank10.mv@gmail.com>
2023-02-08 22:01:38 +0530

    socket io commence

---
+++ public/client.js
@@ -0,0 +1,9 @@
+const socket = io();
+let Name;
+// do {
+// 	Name = prompt("Please Enter Your Name");
+// } while (!Name);
+let textArea = document.querySelector("#text");
+let button = document.getElementById("button");
+
+button.addEventListener("");
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { dirname } = require("path");
+const app = express();
+const Port = process.env.Port || 8000;
+const http = require("http").createServer(app);
+const path = require("path");
+
+http.listen(Port, () => {
+	console.log(`Listening to ${Port}`);
+});
+
+// console.log(path.join(__dirname, "./public"));
+// const staticPath = path.join(__dirname + "/public");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+	console.log("connected!");
+});
