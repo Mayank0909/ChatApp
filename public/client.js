08b1570a0892859158906fc4b4e2c26cd7701be5
Mayank0909 <mayank10.mv@gmail.com>
2023-02-09 14:46:40 +0530

    complete

--- public/client.js
+++ public/client.js
@@ -1,9 +1,53 @@
 const socket = io();
 let Name;
-// do {
-// 	Name = prompt("Please Enter Your Name");
-// } while (!Name);
-let textArea = document.querySelector("#text");
+
+let textarea = document.querySelector("#textarea");
+
 let button = document.getElementById("button");
 
-button.addEventListener("");
+let messageArea = document.querySelector(".message_area");
+do {
+	Name = prompt("Please Enter Your Name : ");
+} while (!Name);
+
+textarea.addEventListener("keyup", (e) => {
+	console.log(e);
+	if (e.key === "Enter") {
+		sendMessage(e.target.value);
+	}
+});
+
+function sendMessage(message) {
+	let msg = {
+		user: Name,
+		message: message.trim(),
+		time: new Date().toLocaleString(),
+	};
+	appendMessage(msg, "outgoing");
+	textarea.value = "";
+	socket.emit("message", msg);
+	scroll();
+}
+
+function appendMessage(msg, type) {
+	let mainDiv = document.createElement("div");
+	let className = type;
+	mainDiv.classList.add(className, "message");
+	let markup = `
+    <h4>${msg.user}</h4>
+    <p class="time">${msg.time}</p>
+
+    <p>${msg.message}</p>
+    `;
+	mainDiv.innerHTML = markup;
+	messageArea.appendChild(mainDiv);
+}
+
+socket.on("message", (msg) => {
+	appendMessage(msg, "incoming");
+	scroll();
+});
+
+function scroll() {
+	messageArea.scrollTop = messageArea.scrollHeight;
+}
